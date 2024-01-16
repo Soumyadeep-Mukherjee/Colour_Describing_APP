11076b944ff2b9cb658b22cf7c63370704cba264
Soumyadeep-Mukherjee <130907775+Soumyadeep-Mukherjee@users.noreply.github.com>
2024-01-17 00:12:24 +0530

    Add files via upload

---
+++ Python_Code_For_Project.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget 
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.metrics import dp
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+class ColourApp(App):
+    pass
+class BoxL(BoxLayout):
+    pass
+class StackL(StackLayout):
+    pass
+class MainWidget(Widget):
+    pass
+class ScrollV(ScrollView):
+    pass
+if __name__=="__main__":
+    ColourApp().run()
